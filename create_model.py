065b8a5b53b4600a3e125d851fa82116fe6978d1
ogoras <skus30@wp.pl>
2022-04-02 18:17:25 +0200

    copied classno_to_char functions

--- create_model.py
+++ create_model.py
@@ -41,7 +41,50 @@ dataset_options = { "numbers": numbers,
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", "-d", help="dataset to use",
     choices=dataset_options.keys(), default="full_dataset")
+parser.add_argument("--input", "-i", help="dataset folder",
+    default="data/images")
 args = parser.parse_args()
 
 dataset = list(dataset_options[args.dataset])
+dataset_folder = args.input
+
+def classno_to_char(ind):
+    polish_letters = list("ĄĆĘŁŃÓŚŹŻ")
+    polish_miniscule_letters = list("ąćęłńóśźż")
+    
+    if ind < 10:
+        return str(ind)
+    elif ind < 36:
+        return chr(ord('A') + ind - 10)
+    elif ind < 45:
+        return polish_letters[ind - 36]
+    elif ind < 71:
+        return chr(ord('a') + ind - 45)
+    elif ind < 80:
+        return polish_miniscule_letters[ind - 71]
+    elif ind < 95:
+        return chr(ord('!') + ind - 80)
+    elif ind < 102:
+        return chr(ord(':') + ind - 95)
+    elif ind < 108:
+        return chr(ord('[') + ind - 102)
+    elif ind < 112:
+        return chr(ord('{') + ind - 108)
+    elif ind == 112:
+        return ' '
+    elif ind == 113:
+        return '\t'
+    else:
+        return "\n"
+
+def classno_to_charname(ind):
+    char = classno_to_char(ind)
+    if char == ' ':
+        return 'space'
+    elif char == '\t':
+        return 'tab'
+    elif char == '\n':
+        return 'newline'
+    else:
+        return char
 

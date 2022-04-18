dce7e71651ff1fee9e4457d51ddcd40b867a02c9
Szymon Kuś <skus30@wp.pl>
2022-04-18 18:08:39 +0200

    created the webservice base

---
+++ webservice.py
@@ -0,0 +1,15 @@
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['POST'])
+def classify():
+    request_data = request.get_json()
+    print(request_data)
+
+    return 'not implemented'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
